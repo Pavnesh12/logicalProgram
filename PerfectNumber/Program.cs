977b6e2ac1a26a962c61a923a849434c753f29a0
Pavnesh12 <pavneshprajapati898@gmail.com>
2021-12-04 16:36:00 +0530

    check the nuber is perfect

---
+++ PerfectNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int perfectNumber = 0;
+            Console.WriteLine("enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            // take input from user;
+            for(int i = 1; i <= number/2; i++)
+            {
+                if(number % 2 == 0)
+                {
+                    perfectNumber += i;
+                }
+                if(number == perfectNumber)
+                {
+                    Console.WriteLine("{0} number is Perfect number", number);
+
+                }
+              
+            }
+           
+
+        }
+    }
+}
